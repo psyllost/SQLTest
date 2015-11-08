60c0f1891cd91bba4658430cf5502c500d0f4390
psyllost <psyllost@gmail.com>
2015-11-08 22:16:30 +0100

    Reading from db and printing into list

--- app/src/main/java/com/plot/ioanna/sqltest/DBCDatabase.java
+++ app/src/main/java/com/plot/ioanna/sqltest/DBCDatabase.java
@@ -1,12 +1,23 @@
 package com.plot.ioanna.sqltest;
 
+import android.app.LoaderManager;
+import android.content.AsyncTaskLoader;
 import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
+import android.database.CursorJoiner;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
 
+import com.google.common.base.Joiner;
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Ioanna on 11/8/2015.
@@ -19,18 +30,19 @@ public class DBCDatabase extends SQLiteAssetHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    public Cursor getBooks() {
-        SQLiteDatabase db = getReadableDatabase();
-        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
-
-        String [] sqlSelect = {"0 _id","book_id"};
-        String sqlTables = "Books";
-
-        qb.setTables(sqlTables);
-        Cursor c = qb.query(db, sqlSelect, null, null,
-                null, null, null);
+    public Cursor getBooks(String userClass, List<String> hasRead) {
+        String hasReadString = "";
+        for (String s : hasRead)
+        {
+            hasReadString += '"'+s+'"' + ",";
+        }
+        hasReadString = hasReadString.substring(0,hasReadString.length()-1);
+        Cursor c = getReadableDatabase().
+                rawQuery("select 0 _id, book_id from Books where user_class LIKE '%" +userClass+ "%'" +
+                        " AND book_id NOT IN ("+hasReadString+")",null);
         Log.i("cursor","  "+ c.getCount());
         c.moveToFirst();
         return c;
     }
+
 }
--- app/src/main/java/com/plot/ioanna/sqltest/MainActivity.java
+++ app/src/main/java/com/plot/ioanna/sqltest/MainActivity.java
@@ -1,35 +1,58 @@
 package com.plot.ioanna.sqltest;
 
 import android.app.ListActivity;
+import android.content.AsyncTaskLoader;
+import android.content.Context;
 import android.database.Cursor;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends ListActivity {
     private Cursor books;
     private DBCDatabase db;
     MainActivity mListActivity;
-
+    List<String> randomBooks= Arrays.asList("870970-basis:27069703", "870970-basis:51039629", "870970-basis:50653463");
+    String userClass="2";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mListActivity=this;
         db = new DBCDatabase(this);
-        books = db.getBooks(); // you would not typically call this on the main thread
-        ListAdapter adapter =new SimpleCursorAdapter(this,
-        android.R.layout.simple_list_item_1,
-                books,
-                new String[] { "book_id"},
-                new int[] {android.R.id.text1});
-        this.setListAdapter(adapter);
+        DatabaseTask task = new DatabaseTask();
+        task.execute(this);
 
-        mListActivity.getListView();
     }
+    private class DatabaseTask extends AsyncTask {
 
+        @Override
+        protected void onPostExecute(Object books) {
+            super.onPostExecute(books);
+            ListAdapter adapter =new SimpleCursorAdapter(mListActivity,
+                    android.R.layout.simple_list_item_1,
+                    (Cursor) books,
+                    new String[]{"book_id"},
+                    new int[] {android.R.id.text1});
+            mListActivity.setListAdapter(adapter);
+
+            mListActivity.getListView();
+        }
+
+        @Override
+        protected Object doInBackground(Object[] params) {
+            Cursor books  = db.getBooks(userClass, randomBooks);
+            return books;
+        }
+    }
 
     @Override
     protected void onDestroy() {
